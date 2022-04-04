176ee002f8aca084a6dfcfee56a1c0455581293a
tariq-ebiz <tariq.mahmood@e-bizsoft.com>
2022-04-04 19:08:58 +0000

    [ADD] Basic Module

---
+++ odoo-academy/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'Odoo Academy',
+    'summary':"""Academy app to manage Training""",
+    'description':"""
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees  
+    """,
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'version': '0.1',
+    'depends': ['base'],
+    'data':[
+        
+        
+    ],
+    'demo':[
+        
+        
+    ],
+    
+}
