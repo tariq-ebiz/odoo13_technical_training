1fcebc1bdeccc5d4b420dbe6fbbf5dc3abbd555d
tariq-ebiz <tariq.mahmood@e-bizsoft.com>
2022-04-06 19:50:54 +0000

    [ADD] Basic module with views and menuitems

--- odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -14,7 +14,8 @@
     'depends': ['base'],
     'data':[
         'security/academy_security.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml'
         
         
     ],
