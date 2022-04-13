787d6f2b96b91ae7b55af2b35ec06023b8a3efbd
tariq-ebiz <tariq.mahmood@e-bizsoft.com>
2022-04-13 19:50:10 +0000

    [ADD] Basic inherit

--- odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -11,13 +11,14 @@
     'author': 'Odoo',
     'website': 'https://www.odoo.com',
     'version': '0.1',
-    'depends': ['base'],
+    'depends': ['sale'],
     'data':[
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
-        'views/session_views.xml', 
+        'views/session_views.xml',
+        'views/sales_views_inherit.xml',
     ],
     'demo':[
         'demo/academy_demo.xml',
